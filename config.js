0bcdf1355fb607332526543c19746923f10ab35e
techzonezw <techzonepvt872.com>
2025-06-04 14:55:38 +0200

    Added GitHub OAuth integration for comments system

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     GITHUB_REPO: 'techzonezw/techzonezw.github.io',
-    GITHUB_CLIENT_ID: 'YOUR_CLIENT_ID',
-    REDIRECT_URI: 'https://techzonezw.github.io'
+    GITHUB_CLIENT_ID: 'Iv23li0QH49IURzwIAyh',
+    REDIRECT_URI: 'https://techzonezw.github.io/webhook'
 };
 
 export default config; 
