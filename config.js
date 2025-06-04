dd4144f0347b696befd967f37d3713a9948a8fb5
techzonezw <techzonepvt872.com>
2025-06-04 15:01:55 +0200

    Updated repository name case

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    GITHUB_REPO: 'techzonezw/techzonezw.github.io',
+    GITHUB_REPO: 'Techzonezw/Techzonezw.github.io',
     GITHUB_CLIENT_ID: 'Iv23li0QH49IURzwIAyh',
     REDIRECT_URI: 'https://techzonezw.github.io/webhook'
 };
